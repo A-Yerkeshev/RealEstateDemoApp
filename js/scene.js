cdd8da4fb3fc18f7c04680c8f45ae197947b4fe2
Arman Yerkesh <a.yerkesh@gmail.com>
2019-04-15 09:45:08 +0300

    Set floor, camera and lights

--- js/scene.js
+++ js/scene.js
@@ -1,22 +1,36 @@
+var scene = new THREE.Scene();
+scene.background = new THREE.Color(0x2D2D2D);
+
+var camera = new THREE.PerspectiveCamera(
+  75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.y = 2;
+camera.position.z = 6;
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
 function init() {
-  var scene = new THREE.Scene();
-  scene.background = new THREE.Color(0x666565);
-
-  var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-  var renderer = new THREE.WebGLRenderer();
-  renderer.setSize( window.innerWidth, window.innerHeight );
-  document.body.appendChild( renderer.domElement );
-
   var ambientLight = new THREE.AmbientLight(0xDAEFFF);
+  scene.add(ambientLight);
   var directionalLight = new THREE.DirectionalLight(0xFFFEE1);
-  scene.add(ambientLight, directionalLight);
+  scene.add(directionalLight);
+
+  var geometry = new THREE.PlaneGeometry( 12, 12 );
+  var material = new THREE.MeshLambertMaterial( {color: 0x2D2D2D} );
+  var floor = new THREE.Mesh( geometry, material );
+  floor.rotateX(-1.5708);
+  scene.add( floor );
+};
+
+function animate() {
+  requestAnimationFrame( animate );
+  render();
 };
 
 function render() {
-  requestAnimationFrame( animate );
   renderer.render( scene, camera );
 };
 
 init();
-render();
+animate();
