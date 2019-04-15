0b8a5f05a475d065920d7000eac3dabec560de23
Arman Yerkesh <a.yerkesh@gmail.com>
2019-04-15 09:49:30 +0300

    Define rotation og the floor

--- js/scene.js
+++ js/scene.js
@@ -10,27 +10,24 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-function init() {
-  var ambientLight = new THREE.AmbientLight(0xDAEFFF);
-  scene.add(ambientLight);
-  var directionalLight = new THREE.DirectionalLight(0xFFFEE1);
-  scene.add(directionalLight);
+var ambientLight = new THREE.AmbientLight(0xDAEFFF);
+scene.add(ambientLight);
+var directionalLight = new THREE.DirectionalLight(0xFFFEE1);
+scene.add(directionalLight);
 
-  var geometry = new THREE.PlaneGeometry( 12, 12 );
-  var material = new THREE.MeshLambertMaterial( {color: 0x2D2D2D} );
-  var floor = new THREE.Mesh( geometry, material );
-  floor.rotateX(-1.5708);
-  scene.add( floor );
-};
+var geometry = new THREE.PlaneGeometry( 12, 12 );
+var material = new THREE.MeshLambertMaterial( {color: 0x2D2D2D} );
+var floor = new THREE.Mesh( geometry, material );
+floor.rotateX(-1.5708);
+scene.add( floor );
+
 
 function animate() {
   requestAnimationFrame( animate );
-  render();
-};
 
-function render() {
+  floor.rotation.z += 0.001;
+
   renderer.render( scene, camera );
 };
 
-init();
 animate();
