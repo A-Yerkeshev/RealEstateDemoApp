bdfe1b82e8f1fa404741603462c1040c35d2e9b5
Arman Yerkesh <a.yerkesh@gmail.com>
2019-04-17 11:58:02 +0300

    Initialize PropertyController.js and PropertyService.js

---
+++ scripts/PropertyController.js
@@ -0,0 +1,3 @@
+Module.controller('PropertyController', ['$scope', function($scope) {
+
+}])
--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,13 @@
-var Module = angular.module('RealEstateApp', []);
+var Module = angular.module('RealEstateApp', ['ngRoute']);
+
+Module.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.
+      when('/', {
+        templateUrl: 'views/main.html'
+      }).
+      when('/buy', {
+        templateUrl: 'views/buy-list.html'
+      }).
+      otherwise('/');
+  }
+]);
